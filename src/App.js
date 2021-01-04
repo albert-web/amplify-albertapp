f28692a3da8117a5f88b4a8f68deeffe69b84d60
albertmensah <DMensah@madisoncollege.edu>
2021-01-04 16:30:02 -0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
   return (
     <div>
       <h1>Hello from AWS Amplify</h1>
+      <h2>Albert created the first amplify project</h2>
+      <p>It was not that bad. Just need to remember the steps</p>
       <AmplifySignOut />
     </div>
   );
